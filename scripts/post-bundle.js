ef538e166aa2d9207b6bbe3c22144d8eb17c7d5e
Unoqwy <julien.qwy@gmail.com>
2021-06-18 13:18:37 +0200

    reduce dist sizes

--- scripts/post-bundle.js
+++ scripts/post-bundle.js
@@ -2,6 +2,7 @@
 
 const fs = require("fs");
 const path = require("path");
+const uglify = require("uglify-js");
 
 const distDir = path.join(__dirname, "..", "dist");
 
@@ -11,9 +12,7 @@ fs.readdirSync(distDir)
     .forEach(fileName => {
         const pluginFile = path.join(distDir, fileName);
         let pluginSources = fs.readFileSync(pluginFile, "utf-8");
-        if (pluginSources.startsWith("(function(){") && pluginSources.endsWith("})();")) {
-            return;
-        }
         pluginSources = `(function(){${pluginSources}})();`;
+        pluginSources = uglify.minify(pluginSources).code;
         fs.writeFileSync(pluginFile, pluginSources, "utf-8");
     });
